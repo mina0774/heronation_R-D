6c08721eb25faf1a8134e278e1052cfb11e1db7c
mina <mina0774@hanmail.net>
2020-04-02 13:56:47 +0900

    Measurement Style Button Event

--- app/src/main/java/com/example/heronation/measurement/Style/MeasurementStyleActivity.java
+++ app/src/main/java/com/example/heronation/measurement/Style/MeasurementStyleActivity.java
@@ -15,6 +15,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MeasurementStyleActivity extends AppCompatActivity {
+    Boolean style1=false,style2=false,style3=false,style4=false,style5=false,style6=false;
     @BindView(R.id.style_1) ImageButton style_1;
     @BindView(R.id.style_2) ImageButton style_2;
     @BindView(R.id.style_3) ImageButton style_3;
@@ -31,7 +32,78 @@ public class MeasurementStyleActivity extends AppCompatActivity {
         style_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                style_1.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                if(style1==false) {
+                    style_1.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                    style1=true;
+                }else {
+                    style_1.clearColorFilter();
+                    style1=false;
+                }
+            }
+        });
+
+        style_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(style2==false) {
+                    style_2.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                    style2=true;
+                }else {
+                    style_2.clearColorFilter();
+                    style2=false;
+                }
+            }
+        });
+
+        style_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(style3==false) {
+                    style_3.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                    style3=true;
+                }else {
+                    style_3.clearColorFilter();
+                    style3=false;
+                }
+            }
+        });
+
+        style_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(style4==false) {
+                    style_4.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                    style4=true;
+                }else {
+                    style_4.clearColorFilter();
+                    style4=false;
+                }
+            }
+        });
+
+        style_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(style5==false) {
+                    style_5.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                    style5=true;
+                }else {
+                    style_5.clearColorFilter();
+                    style5=false;
+                }
+            }
+        });
+
+        style_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(style6==false) {
+                    style_6.setColorFilter(Color.parseColor("#747474"), PorterDuff.Mode.MULTIPLY);
+                    style6=true;
+                }else {
+                    style_6.clearColorFilter();
+                    style6=false;
+                }
             }
         });
 

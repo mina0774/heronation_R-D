b7c91869897ead6aea6b286df18ac52b96b9d6e0
mina <mina0774@hanmail.net>
2020-05-20 09:17:39 +0900

    solve recently viewed item error

--- app/src/main/java/com/example/heronation/wishlist/topbarFragment/WishlistRecentlyViewedItemFragment.java
+++ app/src/main/java/com/example/heronation/wishlist/topbarFragment/WishlistRecentlyViewedItemFragment.java
@@ -52,7 +52,11 @@ public class WishlistRecentlyViewedItemFragment extends Fragment {
 
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("RecentlyViewedItem",MODE_PRIVATE);
 
-        if(sharedPreferences.getString("items", null).equals("{}")) { // 최근 본 상품이 없을 때
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("items",null);
+        editor.commit();
+
+        if(sharedPreferences.getString("items", null)==null||sharedPreferences.getString("items", null).equals("{}")) { // 최근 본 상품이 없을 때
             recycler_view_recently_viewed_item.setVisibility(View.GONE);
             have_not_recently_viewed_item.setVisibility(VISIBLE);
         }
